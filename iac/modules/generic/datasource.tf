66b386a7f98c42b935d5a4b9df4c06cabf3886f5
Francisco Wesley <fwesleyper@gmail.com>
2024-09-29 11:32:01 -0300

    feat(workflows): change workflow to deploy and change values in datasource.tf to integration with cognito

--- iac/modules/generic/datasource.tf
+++ iac/modules/generic/datasource.tf
@@ -1,4 +1,4 @@
 data "aws_cognito_user_pool_client" "existing_client" {
-  user_pool_id = "us-east-1_nIfZvh1Ne"
-  client_id    = "1vgdm90bd4lgoemorpkf9qio3"
+  user_pool_id = "us-east-1_IyxHBlKJN"
+  client_id    = "2olhfuogov0m4bls068uctfubu"
 }
