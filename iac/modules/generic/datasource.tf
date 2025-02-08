07e49ec905fede22b0acfb06e1f73a237cf52a97
Francisco Wesley <fwesleyper@gmail.com>
2025-02-08 15:44:15 -0300

    change workflows, account in module aws_lambda_function and triggers

--- iac/modules/generic/datasource.tf
+++ iac/modules/generic/datasource.tf
@@ -1,4 +1,4 @@
 data "aws_cognito_user_pool_client" "existing_client" {
-  user_pool_id = "us-east-1_IyxHBlKJN"
-  client_id    = "2olhfuogov0m4bls068uctfubu"
+  user_pool_id = "us-east-1_aybz1jBsp"
+  client_id    = "4qoru50dvdqcka83vbdn0vcue8"
 }
